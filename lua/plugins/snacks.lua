20e1cea51c3bdf1d215d7a3ce97700282ad45b17
Jaiden Magnan <jaidenmagnan@gmail.com>
2026-01-21 16:42:05 -0500

    explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,28 +2,29 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	  ---@type snacks.Config
+	---@type snacks.Config
+
+	init = function()
+		_G.Snacks = require("snacks")
+	end,
 	keys = {
 		{ "<leader>fe", false },
 		{ "<leader>fE", false },
-		{ "<leader>E", false },
-		{ "<leader>e", false },
+		{ "<leader>E",  false },
+		{ "<leader>e",  function() Snacks.explorer.reveal() end, desc = "Toggle Explorer" },
 	},
-  init = function()
-    _G.Snacks = require("snacks")
-  end,
 	opts = {
 		bigfile = { enabled = true },
-    		indent = { enabled = true },
-    		input = { enabled = true },
-    		picker = { enabled = true },
-    		notifier = { enabled = true },
-    		quickfile = { enabled = true },
-    		scope = { enabled = true },
-    		scroll = { enabled = true },
-    		statuscolumn = { enabled = true },
-    		words = { enabled = true },
-		explorer = { enabled = false },
+		indent = { enabled = true },
+		input = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = false },
+		words = { enabled = true },
+		explorer = { enabled = true },
 		dashboard = {
 			enabled = true,
 			preset = {
@@ -35,16 +36,16 @@ return {
 ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
 ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
         ]],
-        -- stylua: ignore
-        keys = {
-         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-         { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-         { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
+				-- stylua: ignore
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+					{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+					{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
 			},
 		},
 	},
