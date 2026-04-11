1858cc37b4c26d037ca26ded08e4a1c67d51a78f
Jaiden Magnan <jaidenmagnan@gmail.com>
2026-04-11 11:23:41 -0400

    fix: colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
-    "sainnhe/gruvbox-material",
+    "catppuccin/nvim",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- load the colorscheme here
-        vim.g.gruvbox_material_background = 'hard'
-        vim.g.gruvbox_material_foreground = 'material' -- options: 'material', 'mix', 'original'
-        vim.cmd([[colorscheme gruvbox-material]])
+        vim.cmd([[colorscheme catppuccin-mocha]])
     end,
     opts = {
         transparent = true,
